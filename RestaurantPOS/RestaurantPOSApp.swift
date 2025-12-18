9d5cab055b720fa8072ec491b38d8326316fea96
tom <tommy.yohanes@icloud.com>
2025-12-18 20:57:21 +0700

    Initial Commit

---
+++ RestaurantPOS/RestaurantPOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantPOSApp.swift
+//  RestaurantPOS
+//
+//  Created by tomtomtom on 12/18/25.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantPOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RestaurantPOSTests/RestaurantPOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantPOSTests.swift
+//  RestaurantPOSTests
+//
+//  Created by tomtomtom on 12/18/25.
+//
+
+import Testing
+@testable import RestaurantPOS
+
+struct RestaurantPOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
